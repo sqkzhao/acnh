9f8270b9158c5dfcc2456c49becc5301987c784f
Kay Zhao <58795757+sqkzhao@users.noreply.github.com>
2020-11-04 21:56:08 -0800

    Update GoogleLoginButton.js

--- client/src/modules/components/GoogleLoginButton.js
+++ client/src/modules/components/GoogleLoginButton.js
@@ -10,7 +10,7 @@ const GoogleLoginButton = () => {
     return (
         <div>
             <GoogleLogin
-                clientId="404067026197-5c2cba9m00d56qhpnnppgfivk92f7vgn.apps.googleusercontent.com"
+                clientId=""
                 buttonText="Login"
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
@@ -22,11 +22,3 @@ const GoogleLoginButton = () => {
     );
 }
 
-export default GoogleLoginButton;
-
-// email: "sqkzhao@gmail.com"
-// familyName: "Zhao"
-// givenName: "Kay"
-// googleId: "110091977266337910406"
-// imageUrl: "https://lh6.googleusercontent.com/-gMEc5fT3_70/AAAAAAAAAAI/AAAAAAAACw0/AMZuucnXxvWKh-e4wEQis9W-XMMOw1KNqg/s96-c/photo.jpg"
-// name: "Kay Zhao"
